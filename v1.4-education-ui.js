65f67689afbbbfb717b8c1c8df7b919c1b36a2ab
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-08 21:49:10 +0330

    chore: retire obsolete v1.4 education UI shim

--- v1.4-education-ui.js
+++
@@ -1,7 +0,0 @@
-(()=>{
-'use strict';
-if(window.__KANJI5_EDU_UI_V1_4__)return;
-const CORE=window.__KANJI5_EDU_CORE__;if(!CORE)return;
-window.__KANJI5_EDU_UI_V1_4__=true;
-import('./v1.5-education-ui.js').catch(()=>{});
-})();
