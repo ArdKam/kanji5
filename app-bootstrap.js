baa62d7f5e24357baab3933403cbe5d290d5cb10
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-23 00:39:42 +0330

    fix: invalidate stale navigation CSS and PWA shell

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -11,6 +11,6 @@ try{
     localStorage.setItem(VERSION_KEY,DATA_VERSION);
   }
 }catch(_){}
-if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=105').catch(()=>{});
+if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=108').catch(()=>{});
 import('./v1.6-session.js').catch(()=>{});
 })();
