c0184f35d7854a43a213f8492fb3297130d3f771
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-22 23:42:22 +0330

    fix: force service worker update for card flip release

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -11,6 +11,6 @@ try{
     localStorage.setItem(VERSION_KEY,DATA_VERSION);
   }
 }catch(_){}
-if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js').catch(()=>{});
+if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=105').catch(()=>{});
 import('./v1.6-session.js').catch(()=>{});
 })();
