a10b3cde9645ddbf3830b75b6e6d0f87434b7f16
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-23 15:04:13 +0330

    fix: force service worker refresh for latest UI

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -11,6 +11,6 @@ try{
     localStorage.setItem(VERSION_KEY,DATA_VERSION);
   }
 }catch(_){}
-if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=109').catch(()=>{});
+if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=114').catch(()=>{});
 import('./v1.6-session.js').catch(()=>{});
 })();
