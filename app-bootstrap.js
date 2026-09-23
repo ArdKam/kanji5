7d3fb243fcaa44ea7bf59353f070b8b8bb990430
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-23 15:11:48 +0330

    fix: invalidate stale PWA session-progress cache

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -11,6 +11,6 @@ try{
     localStorage.setItem(VERSION_KEY,DATA_VERSION);
   }
 }catch(_){}
-if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=114').catch(()=>{});
+if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=115').catch(()=>{});
 import('./v1.6-session.js').catch(()=>{});
 })();
