4180eb418e4339a0bd6e0d537c7495d2a326a67d
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-25 00:47:01 +0330

    fix: register latest account signup release

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -11,6 +11,6 @@ try{
     localStorage.setItem(VERSION_KEY,DATA_VERSION);
   }
 }catch(_){}
-if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=127').catch(()=>{});
+if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=128').catch(()=>{});
 import('./v1.6-session.js').catch(()=>{});
 })();
