b8f295ebcd23e812a8e2da046d01bf208f143e67
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-25 00:27:14 +0330

    fix: refresh service worker after practice fix

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -11,6 +11,6 @@ try{
     localStorage.setItem(VERSION_KEY,DATA_VERSION);
   }
 }catch(_){}
-if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=122').catch(()=>{});
+if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=123').catch(()=>{});
 import('./v1.6-session.js').catch(()=>{});
 })();
