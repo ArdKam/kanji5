553a01b3befdeb75dfc10db28d28f577977df316
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-25 01:10:37 +0330

    fix: register account shell cache v130

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -11,6 +11,6 @@ try{
     localStorage.setItem(VERSION_KEY,DATA_VERSION);
   }
 }catch(_){}
-if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=129').catch(()=>{});
+if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=130').catch(()=>{});
 import('./v1.6-session.js').catch(()=>{});
 })();
