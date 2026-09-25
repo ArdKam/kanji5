a815555e2bb7b19bb8534053a0a05dced559c73e
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-25 09:46:29 +0330

    Use current service worker version

--- react-entry.js
+++ react-entry.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=151').catch(()=>{});
+if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=152').catch(()=>{});
 document.documentElement.classList.add('kanji5-react-default');
 const startupRoot=document.getElementById('kanji5-startup-shell');
 const startupObserver=startupRoot?new MutationObserver(()=>{
