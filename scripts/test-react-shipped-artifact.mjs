fa3b841323c730d16ff548b63ab31b23a6442890
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-25 10:37:46 +0330

    test: correct vocabulary scope guard regex

--- scripts/test-react-shipped-artifact.mjs
+++ scripts/test-react-shipped-artifact.mjs
@@ -22,7 +22,7 @@ try{
 }
 if(/getVocabulary\(t===`fa`\?character:character\)/.test(reactJs)) throw new Error("SHIPPED_REACT_JS_CONTAINS_UNDEFINED_VOCABULARY_CHARACTER_REFERENCE");
 const vocabularyBlock=reactJs.slice(reactJs.indexOf("function VocabularyExamples"),reactJs.indexOf("function je("));
-if(/let e=!0[\\s\\S]{0,300}getVocabulary\\(e\\)[\\s\\S]{0,300}\\[character\\]/.test(vocabularyBlock)) throw new Error("SHIPPED_REACT_JS_VOCABULARY_SCOPE_COLLISION");
+if(/let e=!0[\s\S]{0,300}getVocabulary\(e\)[\s\S]{0,300}\[character\]/.test(vocabularyBlock)) throw new Error("SHIPPED_REACT_JS_VOCABULARY_SCOPE_COLLISION");
 console.log("Shipped React JS contains a valid VocabularyExamples character reference.");
 if(/data-kanji5-account-injected/.test(reactJs)) throw new Error("SHIPPED_REACT_JS_CONTAINS_EMBEDDED_FALLBACK_PATCH");
 console.log("Shipped React JS parses as valid JavaScript.");
