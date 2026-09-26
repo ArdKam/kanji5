f65c7489130137f6377c16e347368a354b3273a8
ArdKam <154088465+ArdKam@users.noreply.github.com>
2026-09-26 15:57:05 +0330

    fix: refresh service worker registration asset

--- app-bootstrap.js
+++ app-bootstrap.js
@@ -11,6 +11,6 @@ try{
     localStorage.setItem(VERSION_KEY,DATA_VERSION);
   }
 }catch(_){}
-if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=131').catch(()=>{});
+if('serviceWorker' in navigator)navigator.serviceWorker.register('./sw.js?v=132').catch(()=>{});
 import('./v1.6-session.js').catch(()=>{});
 })();
